8eb883e5185ab57782f0371bccf30208c55a04d9
Gorkem Okan <167126305+gorky77@users.noreply.github.com>
2024-08-21 08:53:52 -0400

    Add files via upload

---
+++ MathMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    public class MathMethods
+    {
+        // Method to add 10 to the input parameter
+        public static int AddTen(int num)
+        {
+            return num + 10;
+        }
+
+        // Method to multiply the input parameter by 2
+        public static int MultiplyByTwo(int num)
+        {
+            return num * 2;
+        }
+
+        // Method to subtract 5 from the input parameter
+        public static int SubtractFive(int num)
+        {
+            return num - 5;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asking the user for a number to perform math operations on
+            Console.WriteLine("Enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            // Performing the math operations using the methods in MathMethods class
+            int result1 = MathMethods.AddTen(input);
+            int result2 = MathMethods.MultiplyByTwo(input);
+            int result3 = MathMethods.SubtractFive(input);
+
+            // Displaying the results to the user
+            Console.WriteLine("After adding 10: " + result1);
+            Console.WriteLine("After multiplying by 2: " + result2);
+            Console.WriteLine("After subtracting 5: " + result3);
+
+            // Keeping the console window open
+            Console.ReadLine();
+        }
+    }
+}
+
